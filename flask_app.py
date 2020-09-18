6b945780a438dd5cdbb04521a6f15ae3c260410d
halimousine <andihalim@berkeley.edu>
2020-09-18 18:52:53 +0000

    adding html templating

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,15 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
-def hello_world():
-    return 'Welcome to the official Grief to Action page!!!'
+def index():
+    return render_template('main.html')
 
+@app.route('/about')
+def about():
+    return 'Learn more about us.'
